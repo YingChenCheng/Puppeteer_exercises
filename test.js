27f3e3c8dbafe01693cbfd778bd0b54af2442d6b
Ying-Chen Cheng <yingchen@Ying-ChendeMacBook-Pro.local>
2021-01-26 21:47:44 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+var startTime;
+var endTime;
+
+// add all URLs you want to visit here
+const urls = [];
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    startTime = Date.now();
+    for (var i = 0; i < 10; i++){
+        await page.goto(urls[i]);
+        console.log(page._target._targetInfo.url);
+    }
+    await browser.close();
+    endTime = Date.now();
+    console.log((endTime - startTime)/1000 + "s");
+})();
+
+run().catch(console.error.bind(console));
